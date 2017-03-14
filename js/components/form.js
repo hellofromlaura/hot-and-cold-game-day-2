046a30fb6d9fbecd6b03d3cfde97a6c47db1e179
Laura Wolfart <laurawolfart@gmail.com>
2017-03-14 12:43:35 -0400

    started form.js component

---
+++ js/components/form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Form(props) {
+	return (
+		<form>
+			<input type="text" placeholder="Enter your Guess" ref={} />
+			<button type="button" onClick={}>Guess</button>
+		</form>
+	)
+}
