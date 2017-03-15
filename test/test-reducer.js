17dad851acf4ee17bbab28848f4f4abe36cc7fb3
Laura Wolfart <laurawolfart@gmail.com>
2017-03-15 14:31:11 -0400

    built test fot make a guess reducer

--- test/test-reducer.js
+++ test/test-reducer.js
@@ -5,6 +5,7 @@ import * as actions from '../js/actions';
 import reducers from '../js/reducers';
 
 describe('game reducer', function () {
+
 	it('should return the initial state', function () {
 		let result = reducers(undefined, {});
 		
@@ -12,6 +13,16 @@ describe('game reducer', function () {
 		result.feedback.should.eq('Make your guess!');
 		result.correctAnswer.should.be.an('number');
 		result.showInfoModal.should.be.eq(false);
+
+	})
+
+	it('should make sure guess is a number', function () {
+		let guess = 'Hello';
+		let result = reducers(undefined, {
+    		type: 'MAKE_GUESS',
+    		guess: guess
+		});
+		result.feedback.should.eq( 'Please enter a valid number');
 
 	})
 })
