b11f0b4e3adfa769ff705bbc1092f3335ec9e479
Terry Hayes <terrychayes@gmail.com>
2017-03-15 12:42:38 -0400

    Adds tests for actions.

---
+++ test/test-actions.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import chai from 'chai';
+const should = chai.should();
+
+import * as actions from '../js/actions';
+
+
+describe('Action: makeGuess', function(){
+  it('should create an action for making a guess', function() {
+    const expected = {
+      type: 'MAKE_GUESS',
+      guess: 10
+    }
+
+    let result = actions.makeGuess(expected.guess);
+    result.should.deep.equal(expected);
+
+  })
+})
+
+describe('Action: toggleInfoModal', function(){
+  it('should create an action for toggling Modal status', function() {
+    const expected = {
+      type: 'TOGGLE_INFO_MODAL'
+    }
+
+    let result = actions.toggleInfoModal();
+    result.should.deep.equal(expected);
+
+  })
+})
+
+describe('Action: newGame', function(){
+  it('should create an action to start a new game', function() {
+
+    let result = actions.newGame();
+    result.type.should.equal('NEW_GAME');
+    result.correctAnswer.should.be.an('number');
+
+  })
+})
---
+++ test/test-index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import chai from 'chai';
+
+const should = chai.should();
+
+
+// action tests
+// reducer tests
+// middleware tests
+// component tests
